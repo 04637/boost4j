9ac38f9b8d2de99de734607e1cd56458c0afd8e3
04637 <04637@163.com>
2020-11-03 23:33:27 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # boost4j
 An out-of-the-box library for java web.
+
+# 初期需求整理
+1. http工具类
+2. 统一响应体
+3. 加解密 (对比 dll/so 的效率)
---
+++ src/main/java/dev/aid/boost4j/Main.java
@@ -0,0 +1,13 @@
+package dev.aid.boost4j;
+
+/**
+ * todo
+ *
+ * @author 04637@163.com
+ * @date 2020/11/3
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
